8adb77fda1045f976be8e1f689dd50c6b9c5dbc3
Dacian <daciang7@gmail.com>
2017-02-23 21:45:49 +0200

    modif la shoppinglist

--- js/shoppinglist.js
+++ js/shoppinglist.js
@@ -1,13 +1,34 @@
-
 console.info('shopping list init');
 
-function getRow(produs,cantitate){
-    return'<tr>'+
-       '<td>'+produs+ '</td>'+
-       '<td>'+cantitate+'</td>'+
-       '</tr>';
+function getRow(produs, cantitate) {
+    return '<tr>' +
+        '<td>' + produs + '</td>' +
+        '<td>' + cantitate + '</td>' +
+        '</tr>';
 }
-document.getElementsByTagName('tbody')[0].innerHTML=getRow("Paine",1)+getRow("Suc",2);
+
+function showList(shoppingList) {
+
+    var lista = '';
+    for (var i = 0; i < shoppingList.length; i++) {
+        var produs = shoppingList[i];
+        lista += getRow(produs.nume, produs.cantitate);
+    }
+
+    document.getElementsByTagName('tbody')[0].innerHTML = lista;
+}
+
+$.ajax('js/mocks/shoppinglist.json', {
+    success: function (shoppingList) {
+        console.info(shoppingList);
+        showList(shoppingList);
+    }
+
+});
+
+
+
+
 
 
 
